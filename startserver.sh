72197b5778a125d8cc0f0143c17714b70ddbf784
Elijah Mitchell <elicmitc@iu.edu>
2021-10-20 15:29:17 +0000

    updating server output

--- startserver.sh
+++ startserver.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 python3 kill-zombie-server.py>nul 2>&1 #suppress STDOUT and STDERR
 rm nul
+echo "Server IP: "
+hostname -I
 python3.8 server.py & PIDSER=$!
 wait $PIDSER	#process id of server
 echo "server connection terminated."
